c30bfc949c19f9a47707f2a65eaaf14f00e017e4
Fabio Arriabene <arrivabene.fabio@outlook.com>
2022-07-04 09:43:44 +0100

    JS

---
+++ JavaScript/index.js
@@ -0,0 +1,26 @@
+/**
+ * Promisse
+ *
+ * A promessa de que algo irá acontecer
+ * Porderá dar certo ou errado,
+ * mas ir+a acontece
+ *
+ **/
+
+let aceitar = false;
+
+console.log("pedir uber");
+const promessa = new Promise((resolve, reject) => {
+  if (aceitar) {
+    return resolve("pedido chegou");
+  }
+
+  return reject("pedido negado!");
+});
+
+promessa
+  .then((result) => console.log(result))
+  .catch((erro) => console.log(erro))
+  .finally(() => console.log("finalizada"));
+
+console.log("aguardando");
